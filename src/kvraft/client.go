fca3f375c96e610b46c0604d9f84b6b62b7513a8
frankszn <sznfrank@gmail.com>
2019-12-08 20:15:40 +0800

    pass 3A test

--- src/kvraft/client.go
+++ src/kvraft/client.go
@@ -4,10 +4,17 @@ import "labrpc"
 import "crypto/rand"
 import "math/big"
 
-
 type Clerk struct {
 	servers []*labrpc.ClientEnd
 	// You will have to modify this struct.
+
+	// 3A
+	clientId int64 // client id
+
+	lastRequestId int // request instruction id, begin from 1, 初始化为0
+
+	leaderHint int // last known leader id
+
 }
 
 func nrand() int64 {
@@ -21,6 +28,9 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 	ck := new(Clerk)
 	ck.servers = servers
 	// You'll have to add code here.
+	ck.clientId = nrand()
+	ck.lastRequestId = 0
+	ck.leaderHint = 0 // default server is 0
 	return ck
 }
 
@@ -39,6 +49,26 @@ func MakeClerk(servers []*labrpc.ClientEnd) *Clerk {
 func (ck *Clerk) Get(key string) string {
 
 	// You will have to modify this function.
+	requestId := ck.lastRequestId + 1
+	var args GetArgs
+	args.Key = key
+	args.CliendId = ck.clientId
+	args.RequestId = requestId
+
+	for {
+		var reply GetReply
+		ok := ck.servers[ck.leaderHint].Call("KVServer.Get", &args, &reply)
+		//DPrintf("Get return")
+		if !ok || reply.WrongLeader == true {
+			numOfservers := len(ck.servers)
+			ck.leaderHint = (ck.leaderHint + 1) % numOfservers
+			continue
+		}
+
+		ck.lastRequestId = requestId
+		return reply.Value
+	}
+
 	return ""
 }
 
@@ -54,6 +84,26 @@ func (ck *Clerk) Get(key string) string {
 //
 func (ck *Clerk) PutAppend(key string, value string, op string) {
 	// You will have to modify this function.
+	requestId := ck.lastRequestId + 1
+	var args PutAppendArgs
+	args.ClientId = ck.clientId
+	args.RequestId = requestId
+	args.Op = op
+	args.Key = key
+	args.Value = value
+
+	for {
+		var reply PutAppendReply
+		ok := ck.servers[ck.leaderHint].Call("KVServer.PutAppend", &args, &reply)
+
+		if !ok || reply.WrongLeader == true {
+			numOfServers := len(ck.servers)
+			ck.leaderHint = (ck.leaderHint + 1) % numOfServers
+			continue
+		}
+		ck.lastRequestId = requestId
+		return
+	}
 }
 
 func (ck *Clerk) Put(key string, value string) {
--- src/kvraft/server.go
+++ src/kvraft/server.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"raft"
 	"sync"
+	"time"
 )
 
 const Debug = 0
@@ -17,11 +18,16 @@ func DPrintf(format string, a ...interface{}) (n int, err error) {
 	return
 }
 
-
 type Op struct {
 	// Your definitions here.
 	// Field names must start with capital letters,
 	// otherwise RPC will break.
+	Type  string // "Get", "Put", "Append"
+	Key   string
+	Value string
+
+	ClientId  int64 // 客户端id
+	RequestId int   // 指令id
 }
 
 type KVServer struct {
@@ -32,16 +38,92 @@ type KVServer struct {
 
 	maxraftstate int // snapshot if log grows this big
 
-	// Your definitions here.
+	// Your definitions here
+	db     map[string]string
+	result map[int]chan Op
+	ack    map[int64]int
 }
 
+func (kv *KVServer) waitForResult(op Op, timeout time.Duration) bool {
+	index, _, isLeader := kv.rf.Start(op)
+	// DPrintf("index is %d", index)
+	if isLeader == false {
+		return true
+	}
+
+	var wrongLeader bool
+
+	kv.mu.Lock()
+
+	ch, ok := kv.result[index]
+	// DPrintf("result is %v", ch)
+	if !ok {
+		// DPrintf("create a op")
+		ch = make(chan Op, 1)
+		kv.result[index] = ch
+	}
+
+	kv.mu.Unlock()
+
+	select {
+	case entry := <-ch:
+		kv.mu.Lock()
+		wrongLeader = !kv.IsSameOp(entry, op)
+		kv.mu.Unlock()
+
+	case <-time.After(timeout):
+		//DPrintf("timeout")
+		kv.mu.Lock()
+		if kv.IsDuplicateRequest(op.ClientId, op.RequestId) {
+			wrongLeader = false
+		} else {
+			wrongLeader = true
+		}
+		kv.mu.Unlock()
+
+	}
+	kv.mu.Lock()
+	delete(kv.result, index)
+	kv.mu.Unlock()
+	return wrongLeader
+}
 
 func (kv *KVServer) Get(args *GetArgs, reply *GetReply) {
 	// Your code here.
+	op := Op{
+		Type:      "Get",
+		Key:       args.Key,
+		ClientId:  args.CliendId,
+		RequestId: args.RequestId,
+	}
+
+	reply.WrongLeader = kv.waitForResult(op, 500*time.Millisecond)
+
+	if reply.WrongLeader == false {
+		kv.mu.Lock()
+		value, ok := kv.db[args.Key]
+		kv.mu.Unlock()
+		if ok {
+			reply.Value = value
+			return
+		}
+		reply.Err = ErrNoKey
+	}
+
 }
 
 func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 	// Your code here.
+	op := Op{
+		Type:      args.Op,
+		Key:       args.Key,
+		Value:     args.Value,
+		ClientId:  args.ClientId,
+		RequestId: args.RequestId,
+	}
+
+	reply.WrongLeader = kv.waitForResult(op, 500*time.Millisecond)
+
 }
 
 //
@@ -53,6 +135,25 @@ func (kv *KVServer) PutAppend(args *PutAppendArgs, reply *PutAppendReply) {
 func (kv *KVServer) Kill() {
 	kv.rf.Kill()
 	// Your code here, if desired.
+}
+
+//helper function
+func (kv *KVServer) IsSameOp(a Op, b Op) bool {
+	if a.ClientId == b.ClientId && a.Type == b.Type && a.Key == b.Key &&
+		a.Value == b.Value && a.RequestId == b.RequestId {
+		return true
+	}
+	return false
+}
+
+func (kv *KVServer) IsDuplicateRequest(clientId int64, requestId int) bool {
+	appliedRequestId, ok := kv.ack[clientId]
+	if ok == false || requestId > appliedRequestId {
+
+		return false
+	}
+
+	return true
 }
 
 //
@@ -84,6 +185,45 @@ func StartKVServer(servers []*labrpc.ClientEnd, me int, persister *raft.Persiste
 	kv.rf = raft.Make(servers, me, persister, kv.applyCh)
 
 	// You may need initialization code here.
+	kv.db = make(map[string]string)
+	kv.result = make(map[int]chan Op)
+	kv.ack = make(map[int64]int)
+
+	go func() {
+		for msg := range kv.applyCh {
+			if msg.CommandValid == false {
+				continue
+			}
+			op := msg.Command.(Op)
+			//DPrintf("server[%d]'s op is [%v]", kv.me, op)
+
+			kv.mu.Lock()
+
+			if kv.IsDuplicateRequest(op.ClientId, op.RequestId) {
+				kv.mu.Unlock()
+				continue
+			}
+
+			switch op.Type {
+			case "Put":
+				kv.db[op.Key] = op.Value
+			case "Append":
+				kv.db[op.Key] += op.Value
+			}
+			kv.ack[op.ClientId] = op.RequestId
+
+			ch, ok := kv.result[msg.CommandIndex]
+			if ok {
+				ch <- op
+			} else {
+
+				kv.result[msg.CommandIndex] = make(chan Op, 1)
+			}
+
+			kv.mu.Unlock()
+
+		}
+	}()
 
 	return kv
 }
